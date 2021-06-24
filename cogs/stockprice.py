537eb43b0a8d9a1422185868dacd1f65d56019c3
T0w3r <torres.alejo2001@gmail.com>
2021-06-24 16:31:20 -0300

    added total_assets functionality and minor cog improvement
    Added total_assests command and fixed stockprice been an on_message command

--- cogs/stockprice.py
+++ cogs/stockprice.py
@@ -1,28 +1,43 @@
+import os
+
 import discord
+import dotenv
 from discord.ext import commands
 import pandas_datareader as web
 import datetime
+import requests
+
+
 class StockPrice(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    def get_stock_price(self,ticker):
+    def get_stock_price(self, ticker):
         data = web.DataReader(ticker, "yahoo")
         return data['Close'].iloc[-1]
 
-    @commands.Cog.listener()
-    async def on_message(self,message):
-        if message.author == self.client.user:  # Evitar que se responda a si mismo
-            return
-        if message.content.startswith("stockprice"):  # precio de la accion
-            try:
-                if len(message.content.split(" ")) == 2:
-                    ticker = message.content.split(" ")[1]
-                    price = self.get_stock_price(ticker)
-                    await message.channel.send(f"stock price of {ticker} is {price}")
+    def total_assests(self, ticker):
+        dotenv.load_dotenv()
+        API_KEY = os.getenv('API_KEY')
+        company = ticker
+        balance_sheet = requests.get(
+            f'https://financialmodelingprep.com/api/v3/balance-sheet-statement/{company}?limit={2}&apikey={API_KEY}')
+        balance_sheet = balance_sheet.json()
+        total_current_assets = balance_sheet[0]['totalCurrentAssets']
+        return total_current_assets
 
-            except(Exception):
-                await message.channel.send(f"Parece que {ticker} No es un nombre reconocible ")
+    @commands.command()
+    async def stockprice(self, ctx, ticker):
+        try:
+            price = self.get_stock_price(ticker)
+            await ctx.send(f"stock price of {ticker} is {price}")
+        except Exception:
+            await ctx.send(f"Parece que {ticker} No es un nombre reconocible ")
+
+    @commands.command()
+    async def totalAssets(self, ctx, ticker):
+        total_assets = self.total_assests(ticker=ticker)
+        await ctx.send(f"Los Activos Totales de la empresa {ticker} son : {total_assets:,} dolares")
 
 
 def setup(client):
